f01f0e4a34979cd9ff771cc0c183c21bc33b896d
Svsk <sverre.skuland@gmail.com>
2017-03-02 18:42:35 +0100

    Handle delivered and pre allocated packages

--- posten-live-sporing/posten-live-sporing.js
+++ posten-live-sporing/posten-live-sporing.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  Live tracker for Posten Sporing
 // @author       Sverre Skuland
-// @match        http://sporing.posten.no/sporing.html?q=*
+// @match        http*://sporing.posten.no/sporing.html?q=*
 // @grant        none
 // ==/UserScript==
 
@@ -13,25 +13,25 @@
 
     var originalTitle = document.title;
     var newUpdateTitle = "";
-    var updateIntervalTime = 10;
+    var updateIntervalTime = 60;
     var timeRemaining = updateIntervalTime;
-    var currentCity = getCurrentCity();
+    var currentLocation = getCurrentLocation();
 
     var container = document.createElement("div");
     $(container).addClass("box");
-    $(container).html("<b>Liveoppdatering (<span class='time-until-update'>"+timeRemaining+"</span>):</b> <span class='city'>"+currentCity+"</span>");
+    $(container).html("<b>Liveoppdatering (<span class='time-until-update'>"+timeRemaining+"</span>):</b> <span class='location'>"+currentLocation+"</span>");
     $(".sporing-sendingandkolli-main-container").before(container);
     
     var updateInterval = setInterval(function () {
-        var city = getCurrentCity();
-        if (city !== currentCity) {
-            $(container).find(".city").html(city);
+        var location = getCurrentLocation();
+        if (location !== currentLocation) {
+            $(container).find(".location").html(location);
             $(container).css("background-color", "orange");
             $(container).css("color", "white");
             
             newUpdateTitle = "Ny oppdatering!";
         } else {
-            $(container).find(".city").html(city);
+            $(container).find(".location").html(location);
         }
 
         timeRemaining = updateIntervalTime;
@@ -44,17 +44,34 @@
         document.title = getTitle();
     }, 1000);
 
-    function getCurrentCity() {
+    function getCurrentLocation() {
         var wrapper = document.createElement("div");
+        var result = "";
+        
         $(wrapper).html($.ajax({
             url: window.location.href,
             async: false
         }).responseText);
-        var currentCity = $(wrapper).find(".sporing-sendingandkolli-latestevent-date span").html();
-        return $.trim(currentCity.replace(/\r?\n|\r/g,''))
+        var currentLocation = $(wrapper).find(".sporing-sendingandkolli-latestevent-date span").html();
+        
+        if (currentLocation) {
+            result = $.trim(currentLocation.replace(/\r?\n|\r/g,''));
+        } else if ($(wrapper).find("div:contains(Ingen sending er mottatt)").length > 0) {
+            result = "Kun forhåndsmeldt";
+        } else if ($(wrapper).find("div:contains(Sendingen er utlevert)").length > 0) {
+            result = "Sendingen er utlevert";
+            stopChecking();
+        }
+        
+        return result;
     }
     
     function getTitle() {
         return newUpdateTitle + " (" + timeRemaining + ") " + originalTitle;
+    }
+    
+    function stopChecking() {
+        clearInterval(timeUntilUpdateInterval);
+        clearInterval(updateInterval);
     }
 })();
